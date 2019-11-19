8b4bab765eaa6fe0f30203ff21e8ecf8b20272a1
Javier L <135663+xleyba@users.noreply.github.com>
2019-11-19 20:08:21 +0100

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> Self {
+        KvStore
+    }
+
+    pub fn set(&self, key: String, value: String) {
+        panic!();
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!();
+    }
+
+    pub fn remove(&self, key: String) {
+        panic!();
+    }
+
+}
